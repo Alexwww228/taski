18951c1041552109155c2daead3c063b5276a558
Alexwww228 <anboygisov@gmail.com>
2025-11-03 02:10:02 +0300

    sss

--- backend/backend/settings.py
+++ backend/backend/settings.py
@@ -102,7 +102,8 @@ USE_TZ = True
 # Static files (CSS, JavaScript, Images)
 # https://docs.djangoproject.com/en/3.2/howto/static-files/
 
-STATIC_URL = '/static/'
+STATIC_URL = '/static_backend/'
+STATIC_ROOT = BASE_DIR / 'static_backend'
 
 # settings.py
 STATICFILES_DIRS = [
